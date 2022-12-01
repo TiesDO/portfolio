4cb7b7b21f6542e7a70117449f7917ee2d303f87
TiesDO <denouden.ties@gmail.com>
2022-12-01 17:08:22 +0100

    add collapse behaviour on icon bubbles

--- script.js
+++ script.js
@@ -21,12 +21,36 @@ const initializeIcons = () => {
 
 		// make the container visible
 		descriptions.classList.add('active');
+
+		// swap the onclick listener to close
+		e.currentTarget.removeEventListener('click', expandIcon);
+		e.currentTarget.addEventListener('click', collapseIcon);
+	};
+
+	const collapseIcon = (e) => {
+		e.currentTarget.classList.remove('active');
+
+		let target = e.currentTarget.dataset.toggleFor;
+
+		// toggle the correct tool description
+		descriptions
+			.querySelector(`div[data-text-for='${target}']`)
+			.classList.remove('active');
+
+		// make the container visible
+		descriptions.classList.remove('active');
+
+		// swap the event listener
+		e.currentTarget.removeEventListener('click', collapseIcon);
+		e.currentTarget.addEventListener('click', expandIcon);
 	};
 
 	icons.forEach((icon) => {
 		icon.addEventListener('click', expandIcon);
 	});
 };
+
+close();
 
 // run app
 const run = () => {
